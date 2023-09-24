6a8cc9ee9023aab5e39d4dd41f868b6321fc738f
MaaX59 <Maax.strandberg@gmail.com>
2023-09-24 20:11:29 +0200

    more navbar

--- src/containers/Navbar/navbar.jsx
+++ src/containers/Navbar/navbar.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import "./navbar.css"
+import React from "react";
+import {FaConnectdevelop} from "react-icons/fa"
+import "./navbar.css";
 
-const Navbar = ()=>{
+const Navbar = () => {
   return (
-    <div className='app__navbar'>
-      <div className='app__navbar-title'>
+    <div className="app__navbar">
+      <div className="app__navbar-title">
+        <FaConnectdevelop size={30}/>
         <h1>Max Strandberg</h1>
       </div>
-      <div className='app__navbar-links'>
-        
-      <h1>Frontend</h1>
-      <h1>Fullstack</h1>
-      <h1>Contact</h1>
-      
-
-
+      <div className="app__navbar-links">
+        <h1>Frontend</h1>
+        <h1>Fullstack</h1>
+        <h1>Technologies</h1>
+        <h1>Contact</h1>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
