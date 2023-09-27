a836b40d20f450b9a557c8624050efc489de3838
MaaX59 <maax.strandberg@gmail.com>
2023-09-27 17:34:55 +0200

    header work

--- src/containers/Header/header.jsx
+++ src/containers/Header/header.jsx
@@ -5,7 +5,7 @@ const Header = ()=>{
   return (
     <div className='app__header'>
     <div className='app__header-intro' >
-      <h1>Hi there!</h1>
+      <h1>Hi there! I´m Max, welcome to my portfolio!</h1>
     </div>
 
     </div>
