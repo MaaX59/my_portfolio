80119be8c0921d839aef28bcdb494f871f5da169
MaaX59 <maax.strandberg@gmail.com>
2023-09-28 18:07:05 +0200

    navbar finished

--- src/containers/Navbar/navbar.jsx
+++ src/containers/Navbar/navbar.jsx
@@ -1,41 +1,38 @@
 import React from "react";
-import {FaConnectdevelop} from "react-icons/fa"
+import { FaConnectdevelop, FaGithub, FaLinkedinIn } from "react-icons/fa";
+
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="app__navbar">
-    <div className="app__navbar-content">
-      <div className="app__navbar-title">
-        <FaConnectdevelop size={30}/>
-        <h1>Max Strandberg</h1>
-      </div>
-      <div className="app__navbar-links">
-      <ul>
-        <li>
-          <a href="#frontend">Frontend</a>
-        </li>
-        <li>
-          <a href="#fullstack">Fullstack</a>
-        </li>
-        <li>
-          <a href="#technologies">Technologies</a>
-        </li>
-        
-      </ul>
-        
-        
-      </div>
+      <div className="app__navbar-content">
+        <div className="app__navbar-title">
+          <FaConnectdevelop size={30} />
+          <h1>Max Strandberg</h1>
+        </div>
+        <div className="app__navbar-links">
+          <ul>
+            <li>
+              <a href="#frontend">Frontend</a>
+            </li>
+            <li>
+              <a href="#fullstack">Fullstack</a>
+            </li>
+            <li>
+              <a href="#technologies">Technologies</a>
+            </li>
+          </ul>
+        </div>
 
-
-      <div className="app__navbar-contact">
-      <div className="app__navbar-contact-content">
-        <h3>contact</h3>
-        {/* https://codepen.io/dig-lopes/pen/WLVGda  use this with github linked in and */}
-      </div>
-      </div>
-
-
+        <div className="app__navbar-contact">
+          <a href="https://github.com/MaaX59">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit">
+            <FaLinkedinIn />
+          </a>
+        </div>
       </div>
     </div>
   );
