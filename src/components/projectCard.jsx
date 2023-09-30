7dc1475f6492f5f78587fb504e6371a0bbec138f
MaaX59 <maax.strandberg@gmail.com>
2023-09-30 16:40:59 +0200

    more card progress

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./projectCard.css";
-import pic from '../images/finedine.png'
+import pic from "../images/finedine.png";
 
 const projectCard = ({ project }) => {
-  const { name, description, img, link, code } = project;
+  const { name, description, link, code } = project;
   return (
     <div className="app__projectCard">
       <div className="app__projectCard-constent">
         <div className="app__projectCard-constent_img">
-
           <img src={pic} alt="project w" />
-
         </div>
-        <div className="app__projectCard-constent_name">
-        <h1>{name}</h1>
+        <div className="app__projectCard-content_name">
+          <h1>{name}</h1>
         </div>
 
         <div className="app__projectCard-constent_description">
           <h5>{description}</h5>
-          
         </div>
 
         <div className="app__projectCard-links">
