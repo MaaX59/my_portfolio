5ef7f71a882907fcb2c9fa97364e05e584c631aa
MaaX59 <maax.strandberg@gmail.com>
2023-10-04 19:33:47 +0200

    navbar mobile finished

--- src/containers/Navbar/navbar.jsx
+++ src/containers/Navbar/navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import {React, useState} from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { FaConnectdevelop, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { SlClose } from "react-icons/sl";
 
 import "./navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="app__navbar">
       <div className="app__navbar-content">
@@ -34,7 +38,36 @@ const Navbar = () => {
           </a>
         </div>
       </div>
-    </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu className="app__navbar-smallscreen-menu"
+        
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen-overlay flex__center slide-bottom">
+            <SlClose
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen-links">
+            <li>
+              <a href="#frontend">Frontend</a>
+            </li>
+            <li>
+              <a href="#fullstack">Fullstack</a>
+            </li>
+            <li>
+              <a href="#technologies">Technologies</a>
+            </li>
+            </ul>
+          </div>
+          )}
+          </div>
+          </div>
+          
+    
   );
 };
 
