a43ceb821f3b6e9ee6321f732331efca7bbe7941
MaaX59 <maax.strandberg@gmail.com>
2024-04-03 17:21:05 +0200

    all done?

--- src/containers/Fullstack/fullstack.jsx
+++ src/containers/Fullstack/fullstack.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import "./fullstack.css"
+import React from "react";
+import "./fullstack.css";
 import data from "../../data/fullstack";
-import ProjectCard from '../../components/projectCard';
+import ProjectCard from "../../components/projectCard";
 
-const Fullstack = ()=>{
+const Fullstack = () => {
   return (
-    <div className='app__fullstack' id='fullstack'>
-      <div className='app__fullstack-title'>
-        <h1>Fullstack</h1>
+    <div className="app__fullstack" id="fullstack">
+      <div className="app__fullstack-title">
+        <h1 className="title">Fullstack</h1>
       </div>
-      <div className='app__fullstack-content'>
-      
-        {data.map((project, index)=>
-
-        <ProjectCard project={project} key={index}/>)}
+      <div className="app__fullstack-content">
+        {data.map((project, index) => (
+          <ProjectCard project={project} key={index} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Fullstack;
